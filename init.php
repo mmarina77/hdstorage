810c404e2e7fa2873dc44abd8c9be52c3ca45f27
Marina <mmelkony@sourcio.com>
2014-12-08 16:26:40 +0400

    Update init.php

--- init.php
+++ init.php
@@ -12,7 +12,7 @@ use WindowsAzure\Common\ServiceException;
 // secondary key = CvZfAdWSOFLjStdQhb8GBkHtA/qag44OYJ/YOfa5wsjJ2V3u3izuYGk4BSQwKgQm4Dx+ufq7ynlnCkKa6FcqZA==
 
 //$connectionString = "DefaultEndpointsProtocol=http;AccountName=hdstorage123;AccountKey=WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==";
-$connectionString = "DefaultEndpointsProtocol=http;AccountName=hdstorage123;AccountKey=WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==";
+$connectionString = "DefaultEndpointsProtocol=http;AccountName=hdstoragesrv;AccountKey=WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==";
 
 $tableRestProxy = ServicesBuilder::getInstance()->createTableService($connectionString);
 echo "$tableRestProxy<br>";
