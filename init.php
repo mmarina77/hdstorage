3c260bf246d7d73aef0bfedd8bcc2e4ba53b2b96
Marina <mmelkony@sourcio.com>
2014-12-08 16:12:53 +0400

    Update init.php

--- init.php
+++ init.php
@@ -6,13 +6,13 @@ echo "**** init.php<br>";
 // name spaces list
 use WindowsAzure\Common\ServicesBuilder;	// wrapper 
 use WindowsAzure\Common\ServiceException;
-
+echo "**** init.php<br>";
 // storage name = hdstorage123
 // primary key = WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==
 // secondary key = CvZfAdWSOFLjStdQhb8GBkHtA/qag44OYJ/YOfa5wsjJ2V3u3izuYGk4BSQwKgQm4Dx+ufq7ynlnCkKa6FcqZA==
 
 //$connectionString = "DefaultEndpointsProtocol=http;AccountName=hdstorage123;Account=WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==";
-$connectionString = "DefaultEndpointsProtocol=http;AccountName=hdstorage123;Account=WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==";
+$connectionString = "DefaultEndpointsProtocol=http;AccountName=hdstoragesrv;Account=WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==";
 
 $tableRestProxy = ServicesBuilder::getInstance()->createTableService($connectionString);
 
