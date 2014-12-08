25f7c6b3eb46c0e037ef476b718cfd8eb1f604db
Marina <mmelkony@sourcio.com>
2014-12-08 16:01:57 +0400

    Update init.php

--- init.php
+++ init.php
@@ -2,6 +2,7 @@
 
 require_once 'vendor/autoload.php';
 
+echo "**** init.php **** <br>";
 // name spaces list
 use WindowsAzure\Common\ServicesBuilder;	// wrapper 
 use WindowsAzure\Common\ServiceException;
@@ -10,6 +11,7 @@ use WindowsAzure\Common\ServiceException;
 // primary key = WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==
 // secondary key = CvZfAdWSOFLjStdQhb8GBkHtA/qag44OYJ/YOfa5wsjJ2V3u3izuYGk4BSQwKgQm4Dx+ufq7ynlnCkKa6FcqZA==
 
+//$connectionString = "DefaultEndpointsProtocol=http;AccountName=hdstorage123;Account=WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==";
 $connectionString = "DefaultEndpointsProtocol=http;AccountName=hdstorage123;Account=WSNmmY8iVisBrhVpQtk+iYdL1AyyLcFlei1gqFAf6o3Fn7XBBwJuColgYGFzW4cAj2yVf/BCCJHlhIrPl2+6cg==";
 
 $tableRestProxy = ServicesBuilder::getInstance()->createTableService($connectionString);
